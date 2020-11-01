b151146e57e996f7cf51a713a954f5a2ae393976
youngscc <1786762249@qq.com>
2020-11-01 12:28:35 +0800

    update

--- 1190200122-袁野-作业3/work.cpp
+++ 1190200122-袁野-作业3/work.cpp
@@ -10,6 +10,8 @@ struct node{
 	node(DataType dat){data = dat;  LeftChild = RightChild = NULL;} 
 };
 
+stack <node*> st;
+
 void RecPreorder (node *now){
 	printf("%d ",now->data);
 	if (now->LeftChild != NULL) RecPreorder(now->LeftChild);
@@ -22,6 +24,78 @@ void RecMidorder (node *now){
 	if (now->RightChild != NULL) RecMidorder(now->RightChild);
 }
 
+void RecPostorder (node *now){
+	if (now->LeftChild != NULL) RecPostorder(now->LeftChild);
+	if (now->RightChild != NULL) RecPostorder(now->RightChild);
+	printf("%d ",now->data);
+}
+
+void NoRecPreorder (node* now){
+	if (now == NULL) return;
+	while (now != NULL || !st.empty())
+	{
+		while (now != NULL)
+		{
+			printf("%d ",now->data);
+			st.push(now);
+			now = now->LeftChild;
+		}
+		
+		if (!st.empty())
+		{
+			now = st.top();
+			st.pop();
+			now = now->RightChild;
+		}
+	}
+}
+
+void NoRecMidorder (node *now){
+	if (now == NULL) return;
+	while (now != NULL || !st.empty())
+	{
+		while (now != NULL)
+		{
+			st.push(now);
+			now = now->LeftChild;
+		}
+		
+		if (!st.empty())
+		{
+			now = st.top();
+			st.pop();
+			printf("%d ",now->data);
+			now = now->RightChild;
+		}
+	}
+}
+
+void NoRecPostorder (node *now){
+	if (now == NULL) return;
+	node *last=NULL;
+	while (now != NULL || !st.empty())
+	{
+		while (now != NULL)
+		{
+			st.push(now);
+			now = now->LeftChild;
+		}
+		
+		if (!st.empty())
+		{
+			now = st.top();
+			if (now->RightChild == NULL || now->RightChild == last)
+			{
+				printf("%d ",now->data);
+				last = now;
+				now = NULL;
+				st.pop();			
+			}
+			else now = now->RightChild;
+		}
+	}
+}
+
 int main(){
 	node *root = new node(1);
 	root->LeftChild = new node(2);
@@ -31,5 +105,13 @@ int main(){
 	root->RightChild->RightChild = new node(7);
 	RecPreorder(root);
 	printf("\n");
+	NoRecPreorder(root);
+	printf("\n");
 	RecMidorder(root);
+	printf("\n"); 
+	NoRecMidorder(root);
+	printf("\n");
+	RecPostorder(root);
+	printf("\n");
+	NoRecPostorder(root);
 }
